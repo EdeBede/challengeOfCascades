0b3c3060adda3ba62a097b95dfff3d869f88c749
yoloZolo <moleratmen@gmail.com>
2021-04-15 11:11:18 +0200

    Added entity manager

--- src/main/java/cascades/DriverApp.java
+++ src/main/java/cascades/DriverApp.java
@@ -1,4 +1,31 @@
 package cascades;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class DriverApp {
+
+    public static void main(String[] args) {
+
+     /*   SessionFactory sessionFactory;
+        Session session;
+        Transaction transaction;
+
+        sessionFactory = HibernateUtil.getSessionFactory();
+        session = sessionFactory.openSession();
+        transaction = session.beginTransaction();*/
+
+        HibernateOperations ho = new HibernateOperations();
+        ho.saveAuthor();
+
+
+    }
+
+    private void setUpAuthorsAndBooks() {
+    }
 }
